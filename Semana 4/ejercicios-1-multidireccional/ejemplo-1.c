b7eb9f6dba4bac2457d7b28a07d2aa8eb9a84d85
Juan Corso <35379369+JhonKophler@users.noreply.github.com>
2021-11-09 21:13:07 -0300

    Semana 4 - Matrices

---
+++ Semana 4/ejercicios-1-multidireccional/ejemplo-1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+
+        int habitantesVivienda[5][25];
+        habitantesVivienda[3][24] = 4;
+
+        printf("Personas que viven en la vivienda 24 del piso 3 es %d \n",habitantesVivienda[3][24]);
+
+}
